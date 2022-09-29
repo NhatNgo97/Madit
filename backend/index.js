4e609fa5faf9047640e097a7e23e89f37fa30cc3
NhatNgo97 <nhatngo9799@gmail.com>
2022-09-29 14:05:40 +1000

    fix cors error

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,25 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const router = require("./routes/index");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 dotenv.config();
 
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("common"));
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGODB_URL, () => {
   console.log("MongoDB Connected");
@@ -22,4 +32,5 @@ app.listen(8000, () => {
   console.log("Server is Running");
 });
 
-app.use("", router);
+//ROUTES
+app.use("/v1", router);
