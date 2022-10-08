04dd832adc9624d650822b85addbe6e18f3bce32
NhatNgo97 <nhatngo9799@gmail.com>
2022-10-09 00:54:45 +1100

    fix cors policy 3

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 dotenv.config();
 
 var corsOptions = {
-  origin: "https://madit-frontend-git-dev-nhatngo97.vercel.app/",
+  origin: "https://madit-frontend-git-dev-nhatngo97.vercel.app",
   optionsSuccessStatus: 200,
   credentials: true,
 };
