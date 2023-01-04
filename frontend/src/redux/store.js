4d034e24fab93a41071a8eb937451a0cf4cd8198
NhatNgo97 <nhatngo9799@gmail.com>
2023-01-04 23:07:36 +1100

    post slice redux (get all posts async thunk )

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import modalSlice from "./modalSlice";
 import authSlice from "./authSlice";
+import postSlice from "./postSlice";
 
 const rootReducer = combineReducers({
   auth: authSlice.reducer,
   modal: modalSlice.reducer,
+  post: postSlice.reducer,
 });
 
 const store = configureStore({
